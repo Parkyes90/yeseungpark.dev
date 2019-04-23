c79b194fe0a8b37493a41fcc8635a114d6659814
parkyes90 <parkyes90@gmail.com>
2019-04-23 09:36:55 +0900

    modify disqus

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,18 +1,15 @@
 module.exports = {
-  siteTitle: 'gatsby-starter-morning-dew',
-  siteDescription: "👋 Hey I'm a Gatsby starter!",
-  authorName: 'Maxence Poutord',
-  twitterUsername: '_maxpou',
+  siteTitle: `YeseungPark's Blog`,
+  siteDescription: `YeseungPark's Tech Blog`,
+  authorName: 'Yeseung Park',
+  twitterUsername: 'none',
   authorAvatar: '/images/avatar.jpeg',
   multilangPosts: true, // enable/disable flags in post lists
   authorDescription: `
-  For the last decade, Maxence Poutord has worked with a variety of web technologies. He is currently focused on front-end development.
-  On his day to day job, he is working as a senior front-end engineer at VSware. He is also a frequent tech speaker and a mentor.
-  As a new digital nomad, he is living where the WIFI and sun is 😎 <br>
-  Do you want to know more? <a href="https://www.maxpou.fr/about" target="_blank">Visit my website!</a>
+  Software Developer
   `,
-  siteUrl: 'https://maxpou.github.io/',
-  disqusSiteUrl: 'https://www.maxpou.fr/',
+  siteUrl: 'https:/yeseungpark.dev/',
+  disqusSiteUrl: 'https://yeseungpark-dev.disqus.com',
   // Prefixes all links. For cases when deployed to maxpou.fr/gatsby-starter-morning-dew/
   pathPrefix: '/gatsby-starter-morning-dew', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpg',
@@ -22,7 +19,7 @@ module.exports = {
   display: 'standalone',
   icon: 'src/assets/gatsby-icon.png',
   postsPerPage: 6,
-  disqusShortname: 'maxpou',
+  disqusShortname: 'yeseungpark-dev',
   headerLinks: [
     {
       label: '🏡',
